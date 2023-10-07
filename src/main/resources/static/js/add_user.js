fd5440f11c78db21f644ddee5b35713df6342ee5
seregakras <seregakras@yandex.ru>
2023-10-07 17:41:43 +0300

    make add and delete scripts

--- src/main/resources/static/js/add_user.js
+++ src/main/resources/static/js/add_user.js
@@ -1,4 +1,5 @@
 async function addUser() {
+    event.preventDefault();
     let select = document.getElementById("add-user-roles");
     let selected = Array.from(select.options)
         .filter(option => option.selected)
@@ -18,4 +19,32 @@ async function addUser() {
         },
         body: JSON.stringify(user)
     });
+
+    document.getElementById("home-tab-pane").classList
+        .add('active', 'show');
+    document.getElementById("profile-tab-pane").classList
+        .remove('active', 'show');
+    document.getElementById("home-tab").classList.add('active');
+    document.getElementById("profile-tab").classList.remove('active');
+
+    let response = await fetch('/rest/admin/');
+    let users = await response.json(); // читаем ответ в формате JSON
+    let html = '';
+    for (let key in users) {
+        html += '<tr>\n' +
+            '<td>' + users[key].id + '</td>\n' +
+            '<td>' + users[key].name + '</td>\n' +
+            '<td>' + users[key].lastName + '</td>\n' +
+            '<td>' + users[key].age + '</td>\n' +
+            '<td>' + users[key].email + '</td>\n' +
+            '<td><div style="float: left; margin-right: 5px"><p>' + users[key].roleIds[0] + '</p></div></td>\n' +
+            '<td><button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#edit' +
+            +users[key].id + '">Edit</button>\n' +
+            '<div th:insert="~{editUser :: edit-user}"></div></td>\n' +
+            '<td><button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete' +
+            +users[key].id + '">Delete</button>\n' +
+            '<div th:insert="~{deleteUser :: delete-user}"></div></td>\n' +
+            '</tr>';
+    }
+    document.getElementById("users-content").innerHTML = html;
 }
--- src/main/resources/static/js/delete_user.js
+++ src/main/resources/static/js/delete_user.js
@@ -1,4 +1,5 @@
 async function deleteUser(event) {
+    event.preventDefault();
     let form = event.target.closest('form');
     let inputs = form.getElementsByClassName('form-control');
     let userId = inputs[0].value;
@@ -9,4 +10,25 @@ async function deleteUser(event) {
             method: 'DELETE',
         });
     }
+
+    let response = await fetch('/rest/admin/');
+    let users = await response.json(); // читаем ответ в формате JSON
+    let html = '';
+    for (let key in users) {
+        html += '<tr>\n' +
+            '<td>' + users[key].id + '</td>\n' +
+            '<td>' + users[key].name + '</td>\n' +
+            '<td>' + users[key].lastName + '</td>\n' +
+            '<td>' + users[key].age + '</td>\n' +
+            '<td>' + users[key].email + '</td>\n' +
+            '<td><div style="float: left; margin-right: 5px"><p>' + users[key].roleIds[0] + '</p></div></td>\n' +
+            '<td><button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#edit' +
+            +users[key].id + '">Edit</button>\n' +
+            '<div th:insert="~{editUser :: edit-user}"></div></td>\n' +
+            '<td><button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete' +
+            +users[key].id + '">Delete</button>\n' +
+            '<div th:insert="~{deleteUser :: delete-user}"></div></td>\n' +
+            '</tr>';
+    }
+    document.getElementById("users-content").innerHTML = html;
 }
