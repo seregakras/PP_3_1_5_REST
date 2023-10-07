a7a1d40882cc24cf6ca8e8af5ecce0e6a8a556ee
seregakras <seregakras@yandex.ru>
2023-10-08 00:33:30 +0300

    reformat html in scripts

--- src/main/resources/static/js/update_user.js
+++ src/main/resources/static/js/update_user.js
@@ -50,96 +50,96 @@ async function updateUser(event) {
 
             // ************** Modal edit window *************************
 
-            '<div class="modal fade" id="edit' + users[key].id + '" tabindex="-1"' +
-            ' aria-labelledby="editModalLabel" aria-hidden="true">' +
-            '<div class="modal-dialog">' +
-            '<div class="modal-content">' +
-            '<div class="modal-header">' +
-            '<h1 class="modal-title fs-5" id="editModalLabel">Edit user</h1>' +
-            '<button type="button" class="btn-close" data-bs-dismiss="modal"' +
-            ' aria-label="Close"></button>' +
-            '</div>' +
-            '<div class="modal-body" id="modal-edit-content">' +
-            '<form name="editUserForm">' +
-            '<div class="mt-3 row g-2">' +
-            '<div class="col-md">' +
-            '<div class="form-floating">' +
-            '<h6><b>ID</b></h6>' +
-            '<input name="id" type="text" value="' + users[key].id + '"' +
-            ' class="form-control" readonly>' +
-            '</div>' +
-            '</div>' +
-            '</div>' +
-            '<div class="mt-3 row g-2">' +
-            '<div class="col-md">' +
-            '<div class="form-floating">' +
-            '<h6><b>First name</b></h6>' +
-            '<input name="name" type="text" value="' + users[key].name + '"' +
-            ' class="form-control" placeholder="Name">' +
-            '</div>' +
-            '</div>' +
-            '</div>' +
-            '<div class="mt-3 row g-2">' +
-            '<div class="col-md">' +
-            '<div class="form-floating">' +
-            '<h6><b>Last name</b></h6>' +
-            '<input name="lastName" type="text" value="' + users[key].lastName + '"' +
-            ' class="form-control" placeholder="Last name">' +
-            '</div>' +
-            '</div>' +
-            '</div>' +
-            '<div class="mt-3 mb-3 row g-2">' +
-            '<div class="col-md">' +
-            '<div class="form-floating">' +
-            '<h6><b>Age</b></h6>' +
-            '<input name="age" type="text" value="' + users[key].age + '"' +
-            ' class="form-control" placeholder="Age">' +
-            '</div>' +
-            '</div>' +
-            '</div>' +
-            '<div class="mt-3 row g-2">' +
-            '<div class="col-md">' +
-            '<div class="form-floating">' +
-            '<h6><b>Email</b></h6>' +
-            '<input name="email" type="text" value="' + users[key].email + '"' +
-            ' class="form-control" placeholder="Email" readonly>' +
-            '</div>' +
-            '</div>' +
-            '</div>' +
-            '<div class="mt-3 row g-2">' +
-            '<div class="col-md">' +
-            '<div class="form-floating">' +
-            '<h6><b>Password</b></h6>' +
-            '<input name="password" type="password"' +
-            ' class="form-control" placeholder="Password">' +
-            '</div>' +
-            '</div>' +
-            ' </div>' +
-            '<div class="mt-3 row g-2">' +
-            '<div class="col-md">' +
-            '<div class="form-floating">' +
-            '<h6><b>Role</b></h6>' +
-            '<select class="form-select" name="roleNames" multiple required>' +
-            '<option value="ADMIN">ADMIN</option>' +
-            '<option value="USER">USER</option>' +
-            '</select>' +
-            '</div>' +
-            '</div>' +
-            '</div>' +
-            '<hr>' +
-            '<div class="bottom-buttons">' +
-            '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">' +
-            'Close' +
-            '</button>' +
-            '<button name="editButton" class="btn btn-primary" onclick="updateUser(event)">' +
-            'Edit' +
-            '</button>' +
-            '</div>' +
-            '</form>' +
-            '</div>' +
-            '</div>' +
-            '</div>' +
-            '</div>' +
+            '<div class="modal fade" id="edit' + users[key].id + '" tabindex="-1"\n' +
+            '     aria-labelledby="editModalLabel" aria-hidden="true">\n' +
+            '    <div class="modal-dialog">\n' +
+            '        <div class="modal-content">\n' +
+            '            <div class="modal-header">\n' +
+            '                <h1 class="modal-title fs-5" id="editModalLabel">Edit user</h1>\n' +
+            '                <button type="button" class="btn-close" data-bs-dismiss="modal"\n' +
+            '                        aria-label="Close"></button>\n' +
+            '            </div>\n' +
+            '            <div class="modal-body" id="modal-edit-content">\n' +
+            '                <form name="editUserForm">\n' +
+            '                    <div class="mt-3 row g-2">\n' +
+            '                        <div class="col-md">\n' +
+            '                            <div class="form-floating">\n' +
+            '                                <h6><b>ID</b></h6>\n' +
+            '                                <input name="id" type="text" value="' + users[key].id + '"\n' +
+            '                                       class="form-control" readonly>\n' +
+            '                            </div>\n' +
+            '                        </div>\n' +
+            '                    </div>\n' +
+            '                    <div class="mt-3 row g-2">\n' +
+            '                        <div class="col-md">\n' +
+            '                            <div class="form-floating">\n' +
+            '                                <h6><b>First name</b></h6>\n' +
+            '                                <input name="name" type="text" value="' + users[key].name + '"\n' +
+            '                                       class="form-control" placeholder="Name">\n' +
+            '                            </div>\n' +
+            '                        </div>\n' +
+            '                    </div>\n' +
+            '                    <div class="mt-3 row g-2">\n' +
+            '                        <div class="col-md">\n' +
+            '                            <div class="form-floating">\n' +
+            '                                <h6><b>Last name</b></h6>\n' +
+            '                                <input name="lastName" type="text" value="' + users[key].lastName + '"\n' +
+            '                                       class="form-control" placeholder="Last name">\n' +
+            '                            </div>\n' +
+            '                        </div>\n' +
+            '                    </div>\n' +
+            '                    <div class="mt-3 mb-3 row g-2">\n' +
+            '                        <div class="col-md">\n' +
+            '                            <div class="form-floating">\n' +
+            '                                <h6><b>Age</b></h6>\n' +
+            '                                <input name="age" type="text" value="' + users[key].age + '"\n' +
+            '                                       class="form-control" placeholder="Age">\n' +
+            '                            </div>\n' +
+            '                        </div>\n' +
+            '                    </div>\n' +
+            '                    <div class="mt-3 row g-2">\n' +
+            '                        <div class="col-md">\n' +
+            '                            <div class="form-floating">\n' +
+            '                                <h6><b>Email</b></h6>\n' +
+            '                                <input name="email" type="text" value="' + users[key].email + '"\n' +
+            '                                       class="form-control" placeholder="Email" readonly>\n' +
+            '                            </div>\n' +
+            '                        </div>\n' +
+            '                    </div>\n' +
+            '                    <div class="mt-3 row g-2">\n' +
+            '                        <div class="col-md">\n' +
+            '                            <div class="form-floating">\n' +
+            '                                <h6><b>Password</b></h6>\n' +
+            '                                <input name="password" type="password"\n' +
+            '                                       class="form-control" placeholder="Password">\n' +
+            '                            </div>\n' +
+            '                        </div>\n' +
+            '                    </div>\n' +
+            '                    <div class="mt-3 row g-2">\n' +
+            '                        <div class="col-md">\n' +
+            '                            <div class="form-floating">\n' +
+            '                                <h6><b>Role</b></h6>\n' +
+            '                                <select class="form-select" name="roleNames" multiple required>\n' +
+            '                                    <option value="ADMIN">ADMIN</option>\n' +
+            '                                    <option value="USER">USER</option>\n' +
+            '                                </select>\n' +
+            '                            </div>\n' +
+            '                        </div>\n' +
+            '                    </div>\n' +
+            '                    <hr>\n' +
+            '                    <div class="bottom-buttons">\n' +
+            '                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">\n' +
+            '                            Close\n' +
+            '                        </button>\n' +
+            '                        <button name="editButton" class="btn btn-primary" onclick="updateUser(event)">\n' +
+            '                            Edit\n' +
+            '                        </button>\n' +
+            '                    </div>\n' +
+            '                </form>\n' +
+            '            </div>\n' +
+            '        </div>\n' +
+            '    </div>\n' +
+            '</div>\n' +
             '</div>' +
 
             // ************************************************************************************
@@ -242,5 +242,8 @@ async function updateUser(event) {
             '</td></tr>';
     }
     document.getElementById("users-content").innerHTML = html;
-    document.querySelectorAll('div.modal-backdrop.fade.show')[0].remove();
+    document.querySelectorAll("div.modal-backdrop.fade.show")[0].remove();
+    document.querySelectorAll("body")[0].classList.remove('modal-open');
+    document.querySelectorAll("body")[0].style.cssText = '';
+
 }
